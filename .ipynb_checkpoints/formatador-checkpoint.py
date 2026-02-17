477cf1819596342f4345646144c1331c86a46a82
Leandro Carísio <carisio@gmail.com>
2026-02-17 08:01:57 -0300

    Adiciona indexação BM25 e alguns testes com qrels gerados por LLM

---
+++ .ipynb_checkpoints/formatador-checkpoint.py
@@ -0,0 +1,28 @@
+import re
+import pandas as pd
+
+def remove_html(html):
+    # Troca nbsp por espaço vazio
+    text = html.replace(' &nbsp; ', ' ')
+    text = text.replace('&nbsp; ', ' ')
+    text = text.replace(' &nbsp;', ' ')
+    text = text.replace('&nbsp;', ' ')
+    return re.sub("<[^>]*>", "", text).strip()
+
+# Além de remover as tags html, tira os &nbsp (troca por espaço) e substitui os parágrafos por \n
+def html_to_plain_text(html: str) -> str:
+    TAG_RE = re.compile(r"<[^>]+>")
+    P_TAG_RE = re.compile(r"<p[^>]*>", flags=re.IGNORECASE)
+
+    if pd.isna(html):
+        return ""
+    # Substitui <p> e <p ...> por nova linha
+    text = P_TAG_RE.sub("\n", html)
+    # Remove todas as outras tags
+    text = TAG_RE.sub("", text)
+    # Troca nbsp por espaço vazio
+    text = text.replace(' &nbsp; ', ' ')
+    text = text.replace('&nbsp; ', ' ')
+    text = text.replace(' &nbsp;', ' ')
+    text = text.replace('&nbsp;', ' ')
+    return text.strip()
